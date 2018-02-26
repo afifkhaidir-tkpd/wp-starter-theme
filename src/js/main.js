3870c7b4f0f2e97ba1fd057e1d00e220e0bbae7a
afifkhaidir-tkpd <khaidir.afif@tokopedia.com>
2018-02-26 19:15:35 +0700

    New output management

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import print from './single';
 
 function component() {
   var element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/js/index.js',
+  entry: {
+    main: './src/js/main.js',
+    single: './src/js/single.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Theme Output'
+    })
+  ],
   output: {
     filename: 'main.min.js',
     path: path.resolve(__dirname, 'dist/js')
